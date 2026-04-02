abb6898f6e5fd5fbbc928390e61c2163c792ff53
Spaghetoz <80428139+Spaghetoz@users.noreply.github.com>
2026-04-02 11:39:54 +0200

    feature: lexing with the logos library

--- src/command_analysis/lexing.rs
+++ src/command_analysis/lexing.rs
@@ -2,36 +2,59 @@
 // Use the RedirectionType enum for both the tokens (in the lexing) and the AST (in the Command enum)
 use crate::command::RedirectionType;
 
-#[derive(Clone, PartialEq, Debug)]
+use logos::Logos;
+
+#[derive(Logos, Debug, PartialEq, Clone)]
+#[logos(skip r"[ \t\n\f]+")] // Ignore this regex pattern between tokens
 pub enum Token {
-    Word(String),
+
+    #[regex(r"<|>>|2>|>", |lex| match lex.slice() {
+        "<" => Some(RedirectionType::In),
+        ">>" => Some(RedirectionType::Append),
+        "2>"  => Some(RedirectionType::Err),
+        ">"  => Some(RedirectionType::Out),
+        _ => unreachable!(),
+    })]
     RedirectOp(RedirectionType),
-    Pipe,
+
+    #[token(";")]
     Separator,
+
+    #[token("&&")]
     And,
+
+    #[token("||")]
     Or,
+
+    #[token("|")]
+    Pipe,
+
+    // Any other string that doesn't matches the previous patterns
+    #[regex(r"[^ \t\n\f;|&<>]+", |lex| lex.slice().to_string())]
+    Word(String),
 }
+
 
 /// Converts an input string into a vec of tokens
 pub fn tokenize_input(input: &str) -> Vec<Token> { 
 
     let mut tokens: Vec<Token> = Vec::new();
 
-    for word in input.split_whitespace() {
-        tokens.push(match word {   
-            "<" => Token::RedirectOp(RedirectionType::In), 
-            ">" => Token::RedirectOp(RedirectionType::Out),
-            ">>" => Token::RedirectOp(RedirectionType::Append),
-            "2>" => Token::RedirectOp(RedirectionType::Err),
-            "|" => Token::Pipe,
-            ";" =>Token::Separator,
-            "||" => Token::Or,
-            "&&" => Token::And,
-            _ => Token::Word(word.to_string())
-        }); 
+    let mut lexer = Token::lexer(input);
+    while let Some(result_token) = lexer.next() {
+        //println!("{:?} {:?}", result_token, lexer.slice());
+        tokens.push(result_token.unwrap()); // TODO do not unwrap and handle error
     }
 
     tokens
+}
+
+
+#[derive(thiserror::Error, Debug)]
+pub enum LexingError {
+
+    #[error("Unexpected token: {0}")]
+    UnknownToken(String),
 }
 
 
@@ -77,13 +100,26 @@ mod tests {
         assert_eq!(expected, result);
     }
 
-    /*#[test]
-    fn test_without_spaces_between_pipe() {
+    #[test]
+    fn test_without_spaces_around_pipe() {
 
-        let input = "ls -l /|cat".to_string();
+        let input = "ls|cat".to_string();
         let result = tokenize_input(&input);
 
-        let expected = vec![Token::Word("ls".into()), Token::Word("/".into()), Token::Pipe, Token::Word("cat".into())];
+        let expected = vec![Token::Word("ls".into()), Token::Pipe, Token::Word("cat".into())];
+
+        assert_eq!(expected, result);
+    }
+
+    // Tests some symbols that should'nt stick each others, 
+    // for instance -l and / should be separated into two words
+    /*#[test]
+    fn specific_symbols_should_not_stick() {
+
+        let input = "-l/|".to_string();
+        let result = tokenize_input(&input);
+
+        let expected = vec![Token::Word("-l".into()), Token::Word("/".into()), Token::Pipe];
 
         assert_eq!(expected, result);
     }*/
